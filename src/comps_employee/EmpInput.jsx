e1ef40ec72009db130242e226335d8cc9e75d72a
אודיה סעדון <osadoun@g.jct.ac.il>
2023-12-14 14:59:04 +0200

    empty input after onclick

--- src/comps_employee/EmpInput.jsx
+++ src/comps_employee/EmpInput.jsx
@@ -8,15 +8,16 @@ const EmpInput = () => {
     const nav = useNavigate();
     const inputRef = useRef();
     const inputRefSearch = useRef();
-    const {gender, setGender} = useContext(AppContext);
+    const { gender, setGender } = useContext(AppContext);
 
     return (
         <div className='container d-flex justify-content-between p-3 m-4 mx-auto'>
             <div>
-            <input ref={inputRef} type="search" placeholder='put in nation' />
-            <button  onClick={() => {
-              nav(`/employee/${inputRef.current.value}`)  
-            }} className='btn btn-info mx-1'>search</button>
+                <input ref={inputRef} type="search" placeholder='put in nation' />
+                <button onClick={() => {
+                    nav(`/employee/${inputRef.current.value}`);
+                    document.querySelector("input").value = "";
+                }} className='btn btn-info mx-1'>search</button>
             </div>
             <div >
                 <select className='form-control' ref={inputRefSearch} onChange={() => {
