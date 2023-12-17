9ff7fe41d7b278964b6c000ce2239faacc991df4
אודיה סעדון <osadoun@g.jct.ac.il>
2023-12-17 19:58:58 +0200

    work sometimes

--- src/featurers/resumeSlice.jsx
+++ src/featurers/resumeSlice.jsx
@@ -12,11 +12,29 @@ const resumeSlice = createSlice({
         addWork: (state, action) => {
             state.works_ar.push(action.payload.work);
         },
+        updateEndTimeWork: (state, action) => {
+            const { id, end } = action.payload;
+          
+            const updatedWorks = state.works_ar.map(work => {
+              if (work.id == id) {
+                return {
+                  ...work,
+                  timeEnd: work.timeEnd == '' ? end : ''
+                };
+              }
+              return work;
+            });
+          
+            return {
+              ...state,
+              works_ar: updatedWorks
+            };
+          },
         addEducation: (state, action) => {
             state.educations_ar.push(action.payload.education);
         }
     }
 })
 
-export const { addWork, addEducation } = resumeSlice.actions;
+export const { addWork, addEducation, updateEndTimeWork } = resumeSlice.actions;
 export default resumeSlice.reducer;
--- src/resume/CompanyItem.jsx
+++ src/resume/CompanyItem.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { addWork } from "../featurers/resumeSlice"
+import { addWork, updateEndTimeWork } from "../featurers/resumeSlice"
 
-const CompanyItem = (props) => {
-    const dispach = useDispatch();
+const CompanyItem = () => {
+    const dispatch = useDispatch();
     const { works_ar } = useSelector(myStore => myStore.resumeSlice);
+    const [id, setId] = useState(Date.now());
 
     const companyNameRef = useRef();
     const timeStartRef = useRef();
     const timeEndRef = useRef();
+    const checkboxRef = useRef();
 
     const handleBlur = () => {
         const companyName = companyNameRef.current.value;
@@ -21,14 +23,25 @@ const CompanyItem = (props) => {
                 companyName,
                 timeStart,
                 timeEnd: timeEnd || '',
-                id: Date.now()
+                id: id
             };
 
-            dispach(addWork({ work: newData }));
-            console.log("ar", works_ar);
+            dispatch(addWork({ work: newData }));
+            console.log(works_ar);
         }
     };
 
+    const handleCheckboxChange = () => {
+        const isChecked = checkboxRef.current.checked;
+        console.log(isChecked);
+        const timeEnd = isChecked ? "yyy" : timeEndRef.current.value;
+    
+        console.log("timeEnd",timeEnd);
+    
+        dispatch(updateEndTimeWork({ id, end: timeEnd }));
+
+        console.log(works_ar);
+      };
 
     return (
         <div className='my-3'>
@@ -40,6 +53,8 @@ const CompanyItem = (props) => {
                 <input onBlur={handleBlur} ref={timeStartRef} type='date' className='form-control me-2' required />
                 <input onBlur={handleBlur} ref={timeEndRef} type='date' className='form-control ms-2' />
             </div>
+            <input ref={checkboxRef} onChange={handleCheckboxChange} className="form-check-input my-1 me-1" type='checkbox'/> 
+            <label className="form-check-label">I'm currently working there.</label>
 
         </div>
     )
