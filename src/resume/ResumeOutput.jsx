adea22781d222362a7688d05544cc78e64a46254
אודיה סעדון <osadoun@g.jct.ac.il>
2023-12-19 16:58:12 +0200

    fix eork exp output

--- src/resume/ResumeOutput.jsx
+++ src/resume/ResumeOutput.jsx
@@ -11,25 +11,28 @@ const ResumeOutput = (props) => {
 
     return (
         <div className='container mx-auto col-md-6'>
-            <div className='header_cv d-flex'>
+            <div className='header_cv d-flex align-items-center'>
                 <div>
                     <img style={imageStyles} className='rounded-circle' src={user.image} alt={user.name} />
                 </div>
                 <div>
                     <h2 className='display-6 mx-3 fw-bold'>{user.name}</h2>
-                    <h4 className='display-6 mx-3'>{user.title}</h4>
+                    <h4 className='lead mx-3'>{user.title}</h4>
 
                 </div>
             </div>
 
             {/* work_exp */}
             <div className='my-3 '>
-                <h2>work experience:</h2>
+                <h4>work experience:</h4>
                 {user.works.map(work => {
                     return <div key={work.id}>
-                        <p><strong>Company name: </strong>{work.company_name}</p>
-                        <p><strong>Role: </strong>{work.role}</p>
-                        <p><strong>From: </strong>{work.time_start}</p>
+                        <p><strong>{work.company_name}</strong></p>
+                        <p>{work.role}</p>
+                        <div className='d-flex'>
+                            <p><strong  className='me-3'> from: </strong> {work.time_start} </p>
+                            {work.time_end != "" && <p><strong  className='mx-3'> to: </strong>{work.time_end}</p>}
+                        </div>
                     </div>
                 })}
             </div>
