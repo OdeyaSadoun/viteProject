23af99573f03b7e3dd1e0f8b04416a1d097edced
אודיה סעדון <osadoun@g.jct.ac.il>
2023-12-19 14:59:18 +0200

    login user to firebase- work

--- src/auto_comps/AutoComp.jsx
+++ src/auto_comps/AutoComp.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Signup from './SignUp'
+import Login from './Login'
 
 const AutoComp = () => {
   return (
     <div>
         <Signup/>
+        <Login/>
     </div>
   )
 }
