c39410e249b8134298f665d69e062b45f07b8024
אודיה סעדון <osadoun@g.jct.ac.il>
2023-12-20 18:37:32 +0200

    little changes

--- src/comps/Home.jsx
+++ src/comps/Home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='container mx-auto text-center my-5'>
+      <h2 className='display-4'>Hello!</h2>
+      <br></br>
+      in this site you can build your cv!
+      <br></br>
+      try it now!
+      <br></br>
+
+      <a className='mx-2' href='/users/login'>Login now</a>
+
+    </div>
   )
 }
 
